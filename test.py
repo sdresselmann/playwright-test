f0645b103b5236b3a17942fa2657b74b4f0d42a7
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-05-03 15:06:55 +0200

    started search for searchword

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 import asyncio
 from playwright.async_api import async_playwright
 
-url = "https://www.duden.de/"
+url = 'https://www.duden.de/'
+searchword = "Präliminarien"
 
 
 async def visitPage():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False, slow_mo=50)
+        browser = await p.firefox.launch(headless=False, slow_mo=50)
         page = await browser.new_page()
         await page.goto(url)
-        await page.fill("#edit-search-api-fulltext--2", "testwort")
+        await page.fill('#edit-search-api-fulltext--2', searchword)
+        await page.click('button:has-text("Nachschlagen")')
         await browser.close()
 
 asyncio.run(visitPage())
