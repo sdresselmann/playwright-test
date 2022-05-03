9f45b4077e416566f09c2e9e8d4c0b83a69522d2
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-05-03 13:53:05 +0200

    refactoring url into variable for reusability

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import asyncio
 from playwright.async_api import async_playwright
 
+url = "https://www.duden.de/"
+
 
 async def visitPage():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False, slow_mo=50)
         page = await browser.new_page()
-        await page.goto("https://www.duden.de/")
+        await page.goto(url)
         await browser.close()
 
 asyncio.run(visitPage())
